6ec653d729e244f4116b810de8e7bd963c64ef7b
Aws <awsdayoub1@gmail.com>
2024-05-19 10:50:50 +0300

    edit urls

--- hotel_service/Hotel/urls.py
+++ hotel_service/Hotel/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('search_for_hotels/<str:word>', views.SearchForHotels.as_view()),
     path('show_hotels/', views.ShowHotels.as_view()),
     path('show_cities/', views.ShowCities.as_view()),
-    path('show_hotel_details/<int:hotel_id>', views.ShowHotelDetails.as_view())
+    #path('show_hotel_details/<int:hotel_id>', views.ShowHotelDetails.as_view())
 ]
