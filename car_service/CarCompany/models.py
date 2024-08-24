3bb83bc0808d516fc1d321c339ad64daeb3b16a9
AwsDayoub <93884262+AwsDayoub@users.noreply.github.com>
2024-08-24 19:30:11 +0300

    Update models.py

--- car_service/CarCompany/models.py
+++ car_service/CarCompany/models.py
@@ -1,14 +1,20 @@
 from django.db import models
-from users.models import User 
+from users.models import User
 from django.utils import timezone
 # Create your models here.
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="city_image" , null=True, blank=True)
 
 class CarCompany(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     phone = models.CharField(max_length=30)
     country = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
+    #city = models.CharField(max_length=50)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
     main_image = models.ImageField(upload_to="car_company_main_image" , null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True , null=True, blank=True)
     sum_of_rates = models.DecimalField(max_digits=7, decimal_places=2 , null=True , blank=True)
@@ -20,7 +26,7 @@ class CarCompany(models.Model):
             return self.sum_of_rates / self.number_of_rates
         else:
             return "null rate"
-        
+
 
     def __str__(self):
         return self.name
@@ -59,30 +65,31 @@ class Car(models.Model):
 
 class CarImages(models.Model):
     car = models.ForeignKey(Car , on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="cars")      
-
+    image = models.ImageField(upload_to="cars")
 
 
 class CarReservation(models.Model):
-    car_company_id = models.ForeignKey(CarCompany , on_delete=models.CASCADE)
-    car_id = models.ForeignKey(Car , on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User , on_delete=models.CASCADE)
+    car_company = models.ForeignKey(CarCompany, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateTimeField(default=timezone.now)
     end_date = models.DateTimeField(default=timezone.now() + timezone.timedelta(minutes=5))
-    pickup_location = models.CharField(max_length=200 , null=True , blank=True)
-    delivery_location = models.CharField(max_length=200 , null=True , blank=True)
-    description = models.TextField(null=True , blank=True)
-
+    pickup_location = models.CharField(max_length=200, null=True, blank=True)
+    delivery_location = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    is_car_reservation = models.BooleanField(default=True)
+    is_hotel_reservation = models.BooleanField(default=False)
     @property
     def number_of_days(self):
         duration = self.end_date - self.start_date
         return duration.days
+
     @property
     def calculate_total_price(self):
-        return self.car_id.price * self.number_of_days
-    
+        return self.car.price * self.number_of_days
+
     def __str__(self):
-        return "reservation_id: " + str(self.pk) + "car_company_id: " + str(self.car_company_id) + " car_id: " + str(self.car_id) + " user_id: " + str(self.user_id)
+        return f"reservation_id: {self.pk}, car_company_id: {self.car_company.id}, car_id: {self.car.id}, user_id: {self.user.id}"
 
 
 class CarReservationIdImage(models.Model):
